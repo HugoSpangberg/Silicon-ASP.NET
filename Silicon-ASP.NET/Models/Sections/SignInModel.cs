b165cff09ad4ab46d4e7de90bf7a1070d3a2781b
hugospangberg <hugospangberg@HUGOSPNGBER517B>
2024-03-05 23:33:37 +0100

    Started create SignIn form

---
+++ Silicon-ASP.NET/Models/Sections/SignInModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Silicon_ASP.NET.Models.Sections;
+
+public class SignInModel
+{
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Invalid email address")]
+    [Display(Name = "Email Address", Prompt = "Enter your email Adress", Order = 0)]
+
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 1)]
+    public string Password { get; set; } = null!;
+
+
+    [Display(Name = "Remeber me", Order = 2)]
+    public bool RememberMe { get; set; }
+}
---
+++ Silicon-ASP.NET/ViewModels/Sections/SignInViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Silicon_ASP.NET.Models.Sections;
+
+namespace Silicon_ASP.NET.ViewModels.Sections;
+
+public class SignInViewModel
+{
+    public string Title { get; set; } = "Sign in";
+
+    public SignInModel Form { get; set; } = new SignInModel();
+
+}
